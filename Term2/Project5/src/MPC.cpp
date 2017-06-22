83abea20b7d6a4b8ab6fd5a4b4462aca08876342
sstuteja <sundeeptuteja@gmail.com>
2017-06-22 11:14:25 -0700

    Finally the car moves (backward)

--- Term2/Project5/src/MPC.cpp
+++ Term2/Project5/src/MPC.cpp
@@ -218,5 +218,5 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   //
   // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
   // creates a 2 element double vector.
-  return {};
+  return {solution.x[0], solution.x[1], solution.x[2], solution.x[3], solution.x[4], solution.x[5], solution.x[6], solution.x[7]};
 }
